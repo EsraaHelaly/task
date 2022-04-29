67f679e157f40a22a9c7a992430271d71f53a346
EsraaHelaly <helalyesraa144@gmail.com>
2022-04-29 20:46:59 +0200

    first commit

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:task/persentation/screens/phone_screen.dart';
+import 'package:task/persentation/screens/Login_screen.dart';
 
 import 'business_logic/cubit/phone_auth_cubit.dart';
 import 'constants/strings.dart';
