20862f499364d35ebf00b054ad185ddc3be076b2
EsraaHelaly <helalyesraa144@gmail.com>
2022-04-30 11:47:29 +0200

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:task/persentation/screens/Login_screen.dart';
 
 import 'app_router.dart';
 import 'constants/strings.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       onGenerateRoute: appRouter.generateRoute,
-      initialRoute: otpScreen,
+      initialRoute: loginScreen,
     );
   }
 }
